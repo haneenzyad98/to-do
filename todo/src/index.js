2413109f3fbeab257964c671cd365135a8ea9771
Haneen zyad odebat <haneenzyad98@gmail.com>
2021-07-03 18:38:18 +0300

    lab31

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+import App from './app.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Main extends React.Component {
+  render() {
+    return <App />;
+  }
+}
 
-
+const rootElement = document.getElementById('root');
+ReactDOM.render(<Main />, rootElement);
